5e947e160320be64591a0cb88ff2a2f64a681752
Max Maischein <corion@corion.net>
2020-09-29 19:37:07 +0200

    Bump version, update Changes, regenerate META.*

--- lib/Spreadsheet/ParseODS.pm
+++ lib/Spreadsheet/ParseODS.pm
@@ -11,7 +11,7 @@ use Carp qw(croak);
 use List::Util 'max';
 #use Storable 'dclone';
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 our @CARP_NOT = (qw(XML::Twig));
 
 use Filter::signatures;
--- lib/Spreadsheet/ParseODS/Styles.pm
+++ lib/Spreadsheet/ParseODS/Styles.pm
@@ -7,7 +7,7 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 use PerlX::Maybe;
 
-our $VERSION = '0.27';
+our $VERSION = '0.28';
 
 =head1 NAME
 
