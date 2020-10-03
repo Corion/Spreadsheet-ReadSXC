753a6a831842001ab3a35036587386cd99875e8e
Max Maischein <corion@corion.net>
2020-10-03 11:35:29 +0200

    Fix usage of "-s _" to placate Filter::signatures / Filter::Simple
    Perl really isn't that easy to parse. -s can also be interpreted as the
    "-" and the start of a s/// replacement

--- lib/Spreadsheet/ParseODS.pm
+++ lib/Spreadsheet/ParseODS.pm
@@ -654,7 +654,7 @@ sub _open_xml_thing( $self, $source, $wb_info, %options ) {
 sub _open_sxc {
     my ($self, $sxc_file, $options_ref) = @_;
     if( !$options_ref->{StrictErrors}) {
-        -f $sxc_file && -s _ or return undef;
+        -f $sxc_file && -s *_ or return undef;
     };
     open my $fh, '<', $sxc_file
         or croak "Couldn't open '$sxc_file': $!";
